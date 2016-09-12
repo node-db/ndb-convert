a733433852ae8f790d52b3ef8cbf06bbfbda371b
Yugeng Hui <interhuiyg@163.com>
2016-09-12 23:44:06 +0800

    Add test case

--- convert.py
+++ convert.py
@@ -6,28 +6,34 @@ Convert ndb
 @author: Huiyugeng
 '''
 
-
 import json
 import py_xml
 
-def xml2json(xml):
-    try:
-        obj = py_xml.parse_string(xml)
-        if obj is not None:
-            return json.dumps(obj)
-    except:
-        pass
+def xml2json(xml_str):
+    obj = py_xml.parse_string(xml_str)
+    if obj is not None:
+        return json.dumps(obj)
     return None
 
-def json2xml(json):
-    try:
-        obj = json.loads(json)
-        if obj is not None:
-            return  py_xml.to_xml(obj)
-    except:
-        pass
+def json2xml(json_str):
+    obj = json.loads(json_str)
+    if obj is not None:
+        return  py_xml.to_xml(obj)
     return None
 
-def convert(ndb, script):
-    return ndb
+def convert(template, data, in_type = 'xml', out_type = 'json'):
+    obj = None
+    if in_type == 'xml':
+        obj = py_xml.parse_string(data)
+    elif in_type == 'json':
+        obj = json.loads(data)
+        
+    
+    if obj:
+        if out_type == 'xml':
+            return py_xml.to_xml(obj)
+        elif out_type == 'json':
+            return json.dump(obj)
+        
+    return None
 
--- convert_test.py
+++ convert_test.py
@@ -1,1 +1,54 @@
 #coding=utf-8
+
+import py_xml
+import convert
+
+xml = '''
+<root>
+    <group name="test">
+        <family>green</family>
+        <family>cooker</family>
+        <start/>
+    </group>
+    <parent name="green">
+        <child name="jim" age="23" sex="male">
+            <address>Shenzhen</address>
+            <phone>18607578001</phone>
+        </child>
+        <child name="lucy" age="21" sex="female" address="Chongqing">
+            <address>Guangzhou</address>
+            <phone>18607578002</phone>
+        </child>
+        <child name="lily" age="21" sex="female">
+            <address>Hangzhou</address>
+            <phone>18607578003</phone>
+        </child>
+    </parent>
+    <parent name="cooker">
+        <child name="tim" age="34" sex="male">
+            <address>Beijing</address>
+            <phone>13704453232</phone>
+        </child>
+        <child name="tom" age="45" sex="male">
+            <address>Shanghai</address>
+            <phone>13391562334</phone>
+        </child>
+    </parent>
+</root>
+'''
+
+json = convert.xml2json(xml)
+print convert.json2xml(json)
+
+template = '''
+<root>
+    <child>
+        ${root->parent->child->name:lily}
+    <child>
+</root>
+'''
+
+ndb = py_xml.parse_string(xml)
+print convert.convert(template, ndb)
+
+
